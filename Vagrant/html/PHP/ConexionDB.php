06cc950698ae1ec719830ebc1fc55da88ce5e307
Samuel García Dominguez <garcia.dominguez.samuel@alumnat.copernic.cat>
2023-05-08 17:29:14 +0200

    Comentado ConexionDB.php

--- Vagrant/html/PHP/ConexionDB.php
+++ Vagrant/html/PHP/ConexionDB.php
@@ -1,12 +1,21 @@
 <?php
+//Se define el nombre del servidor
 if (!defined('DATABASE')) define('DATABASE',   'GExpensesBBDD');
+//Se define el usuario 
 if (!defined('BD_USUARIO')) define('BD_USUARIO',   'gexpensesuser');
+//Se define la contraseña del usuario para conectar a la base de datos
 if (!defined('BD_CLAVE')) define('BD_CLAVE',   '1234');
+//Se define la ubicación del servidor de la base de datos "mysql:host=172.16.0.10", 
+//el nombre "dbname=' . DATABASE ." y la codificación de caracteres "charset=utf8"
 if (!defined('SERVER_MYSQL')) define('SERVER_MYSQL',   'mysql:host=172.16.0.10;dbname=' . DATABASE . ';charset=utf8');
 
+// Se crea una instancia de la clase PDO, que representa una conexión a una base de datos.
+// El constructor de la clase recibe tres parámetros: el DSN (Data Source Name), el usuario y la contraseña.
+// El DSN se construye a partir de la constante SERVER_MYSQL, que contiene la información necesaria para conectar
+// al servidor MySQL y seleccionar la base de datos especificada en la constante DATABASE.
 try {
         $conexion = new PDO(SERVER_MYSQL, BD_USUARIO, BD_CLAVE);
 } catch (Exception $e) {
-
+//En caso de error al conectar a la base de datos, muestra un mensaje de error.
         echo 'Error conectando a la BBDD. ' . $e->getMessage();
 }
--- Vagrant/html/PHP/validacionLoginRegister.php
+++ Vagrant/html/PHP/validacionLoginRegister.php
@@ -1,28 +1,8 @@
-// Validación inputs Login y Registro
 <?php
-function validarDadesFormulari($firstname, $email, $passwordReg, $valid, $conexion)
+include 'ConexionDB.php';
+function validarDadesFormulari($firstname, $email, $passwordReg, $valid)
 {
-    // Consulta SQL para verificar si el nombre de usuario o el correo electrónico ya existen
-    $query = "SELECT COUNT(*) FROM usuario WHERE nombre = :nombre OR email = :email";
-    $consulta = $conexion->prepare($query);
-    $consulta->bindParam(':nombre', $firstname);
-    $consulta->bindParam(':email', $email);
-    $consulta->execute();
-    $count = $consulta->fetchColumn();
-
-    //verifica si, la consulta SQL para comprobar si el nombre de usuario o el correo electrónico ya existen
-    //en la base de datos, ha devuelto al menos una fila. Si la ha devuelto, quiere decir que el  registro no 
-    //es valido
-    if ($count > 0) {
-        $valid = false;
-        echo "El nombre de usuario o el correo electrónico ya están en uso.";
-        return false;
-    }
-
-    //Expresion regular contraseña: 8 caracteres, 1 mayuscula, 1 minuscula y 1 caracter especial.
     $password_regex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/";
-
-    //Si los inputs del registro o login están vacios, manda mensaje de error
     if ((empty($firstname))) {
         $valid = false;
     }
@@ -41,8 +21,6 @@ function validarDadesFormulari($firstname, $email, $passwordReg, $valid, $conexi
     return $valid;
 }
 
-//Si los inputs no están vacios, se recogen los valores enviados para el nombre de usuario, 
-//correo electrónico y contraseña. 
 if ((!empty($_POST))) {
 
     $firstname = $_POST["username"];
@@ -51,16 +29,11 @@ if ((!empty($_POST))) {
 
     $valid = true;
 
-    //Se llama a la función validarDadesFormulari para validar los datos del formulario. 
-    $resultatvalidacio = validarDadesFormulari($firstname, $email, $passwordReg, $valid, $conexion);
+    $resultatvalidacio = validarDadesFormulari($firstname, $email, $passwordReg, $valid);
 
-    //Si esta función devuelve true, todos los campos del formulario han pasado la validación
+    $registered = false;
     if ($resultatvalidacio == true) {
-
-        //Generamos con la funcion password_hash un hash de la contraseña del usuario
         $hash_password = password_hash($passwordReg, PASSWORD_DEFAULT);
-
-        //Guardamos los datos en la tabla usuario de la base de datos
         $query = "INSERT INTO usuario (nombre,email,contrasena) VALUES (:nombre,:email,:contrasena)";
 
         $consulta = $conexion->prepare($query);
@@ -74,15 +47,12 @@ if ((!empty($_POST))) {
     }
 }
 
-//Si el usuario ha dado click en el botón Login
 if ((isset($_POST['buttonLogin']))) {
 
-    //Obtenemos los datos del formulario username y password
     $nameuserL = $_POST['usernameLogin'];
 
     $passwordL = $_POST['passwordLogin'];
 
-    //Se hashea la contraseña introducida en el formulario de login
     $hash_passwordLogin = password_hash($passwordL, PASSWORD_DEFAULT);
 
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -95,19 +65,13 @@ if ((isset($_POST['buttonLogin']))) {
 
     $user = $queryLogin->fetch(PDO::FETCH_ASSOC);
 
-    //Si el nombre de usuario no existe en la base de datos, manda mensaje de error
     if (!$user) {
         echo '<div class="error-message">El usuario no existe en la base de datos.</div>';
-
-    //Se verifica si la contraseña proporcionada por el usuario coincide con la contraseña encriptada 
-    //en la base de datos con la funcion password_verify()
     } else if (password_verify($passwordL, $user['contrasena'])) {
         $_SESSION['usuario'] = $nameuserL;
         $_SESSION['id_usuario'] = $user['id_usuario'];
 
         header("location: PHP/Home.php");
-
-    //Si la contraseña no coincide con la de la base de datos, manda un mensaje de error
     } else {
         echo '<div class="error-message">Contraseña incorrecta.</div>';
     }
